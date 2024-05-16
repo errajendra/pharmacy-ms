46827c0578e7bac095f48633116de5731c0fcfff
rajendraswooshelf <rajendras@wooshelf.com>
2024-05-16 19:33:44 +0530

    changes

--- appointment/urls.py
+++ appointment/urls.py
@@ -5,5 +5,6 @@ from .views import *
 urlpatterns = [
     path("admin-appointment-list/", appointment_list, name="appointment_list_admin"),
     path("admin-appointment-book/", new_appointment, name="new_appointment_admin"),
-
+    path("admin-appointment-update/<int:id>/", edit_appointment, name="edit_appointment_admin"),
+    path("admin-appointment-delte/<int:id>/", delete_appointment, name="delete_appointment_admin"),
 ]
--- appointment/views.py
+++ appointment/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from pharmacy.models import Patients, Doctor
 from .models import Appointment
 from .forms import AppointmentForm
@@ -8,7 +9,8 @@ def appointment_list(request):
     appointments = Appointment.objects.all().order_by('-date')
     context = {
         "title": "Appointments",
-        "appointments": appointments
+        "appointments": appointments,
+        "doctors": Doctor.objects.all()
     }
     return render(request, "appointment/list.html", context)
 
@@ -19,10 +21,36 @@ def new_appointment(request):
         if form.is_valid():
             form.save()
             return redirect("appointment_list_admin")
+        else:
+            messages.warning(request, str(form.errors))
     context = {
         "title": "Book New Appointment",
-        "form": form,
+        # "form": form,
         "patients": Patients.objects.all(),
         "doctors": Doctor.objects.all()
     }
     return render(request, "appointment/new_booking.html", context)
+
+
+def edit_appointment(request, id):
+    appointment = Appointment.objects.get(id=id)
+    if request.method == "POST":
+        form = AppointmentForm(request.POST or None, instance=appointment)
+        if form.is_valid():
+            form.save()
+            return redirect("appointment_list_admin")
+        else:
+            messages.warning(request, str(form.errors))
+    context = {
+        "title": "Update Appointment",
+        "instance": appointment,
+        "patients": Patients.objects.all(),
+        "doctors": Doctor.objects.all()
+    }
+    return render(request, "appointment/new_booking.html", context)
+
+
+def delete_appointment(request, id):
+    appointment = Appointment.objects.get(id=id)
+    appointment.delete()
+    return redirect("appointment_list_admin")
