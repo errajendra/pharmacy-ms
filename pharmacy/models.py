8576b1aef8863c9ceb0410348337c022bc034ece
rajendraswooshelf <rajendras@wooshelf.com>
2024-05-30 12:12:40 +0530

    changes

--- pharmacy/models.py
+++ pharmacy/models.py
@@ -504,7 +504,7 @@ class Stock(BaseModel):
     quantity = models.IntegerField(verbose_name="Total Quantity", default=0, blank=True, null=True)
     batch = models.CharField(max_length=50, blank=True, null=True)
     actual_price = models.FloatField(default=0, blank=True, null=True, verbose_name="Actual Price")
-    price = models.FloatField(default=0, blank=True, null=True, verbose_name="M.R.P")
+    price = models.FloatField(default=0, verbose_name="M.R.P/Unit")
     
     # drug_color = models.CharField(max_length=50, blank=True, null=True)
     # batch_number = models.CharField(max_length=50, blank=True, null=True)
@@ -567,7 +567,7 @@ class Stock(BaseModel):
     def quantity_price(self):
         if self.price:
             try:
-                return self.price/self.unit
+                return self.price/self.unit_quantity
             except:
                 pass
         return 0
